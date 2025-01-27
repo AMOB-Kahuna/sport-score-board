1acef626fd282f39f519cebe6a9c950a63894785
AMOB-Kahuna <balloabdulmujeeb42@gmail.com>
2025-01-27 02:42:22 +0100

    add script for incrementing score board

---
+++ index.js
@@ -0,0 +1,34 @@
+let homeScore = 0
+let guestScore = 0
+let homeBoard = document.getElementById("home-score")
+let guestBoard = document.getElementById("guest-score")
+
+function homePlus1() {
+    homeScore += 1
+    homeBoard.textContent = homeScore
+}
+
+function homePlus2() {
+    homeScore += 2
+    homeBoard.textContent = homeScore
+}
+
+function homePlus3() {
+    homeScore += 3
+    homeBoard.textContent = homeScore
+}
+
+function guestPlus1() {
+    guestScore += 1
+    guestBoard.textContent = guestScore
+}
+
+function guestPlus2() {
+    guestScore += 2
+    guestBoard.textContent = guestScore
+}
+
+function guestPlus3() {
+    guestScore += 3
+    guestBoard.textContent = guestScore
+}
